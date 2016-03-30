04d602c9406fdaf7921398c6b75ec2ffbdd33914
Steve T <stevethewebguy@gmail.com>
2016-03-30 13:59:55 -0700

    Dynamic routes

--- countries-capitals/app/cc-app.js
+++ countries-capitals/app/cc-app.js
@@ -3,26 +3,33 @@ ccApp.config(function($stateProvider, $urlRouterProvider){
 
     // For any unmatched url, send to home
     $urlRouterProvider.otherwise("/");
-
     $stateProvider
         .state('home', {
             url: "/",
             templateUrl: "home.html"
         })
         .state('countryList', {
-            url: "/country-list",
+            url: "/countries",
             templateUrl: "country-list.html"
         })
         .state('countryDetail', {
-            url: "/country-detail",
-            templateUrl: "country-detail.html"
+            url: "/countries/:country",
+            templateUrl: "country-detail.html",
+            controller: function($scope, $stateParams){
+                $scope.country = $stateParams.country;
+            },
+            resolve:{
+                country: ['$stateParams', function($stateParams){
+                    return $stateParams.country;
+                }]}
         })
-        //.state('route1.list', {
-        //    url: "/list",
-        //    templateUrl: "route1.list.html",
-        //    controller: function($scope){
-        //        $scope.items = ["A", "List", "Of", "Items"];
-        //    }
-        //})
+        .state('countryDetail.capital', {
+            url: "/capital",
+            templateUrl: "country-detail.html",
+            controller: function($scope, $stateParams, country){
+                $scope.country = country;
+            }
+        });
+
 
 });
