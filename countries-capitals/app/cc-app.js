abf1fdcbca37bdf3f8a7244384a909920e32c4d5
Steve T <stevethewebguy@gmail.com>
2016-04-09 00:06:52 -0700

    Country list caching working

--- countries-capitals/app/cc-app.js
+++ countries-capitals/app/cc-app.js
@@ -32,10 +32,19 @@ angular.module('ccApp', ['ui.router', 'ngAnimate'])
         })
         .state('countries', {
             url: "/countries",
-            templateUrl: "country-list.html"
+            controller: 'countryCtrl',
+            templateUrl: "country-list.html",
+            resolve: {
+                importcountries: function (api2) {
+                    //if there's a cached version saved in api2 don't load a new one when the state is loaded
+                    if(!api2.cache){
+                        api2.cache = api2.importCountries()
+                    }
+                    return api2.cache
+                }
+            }
         })
 
-        //is this how you have to do it? id rather not have to call county and pass along stuff to the child
         .state('country', {
             url: "/countries/:country/capital",
             templateUrl: "country-detail.html",
@@ -43,8 +52,15 @@ angular.module('ccApp', ['ui.router', 'ngAnimate'])
             resolve: {
                 neighbors: function(api, $stateParams){
                     return api.searchNeighbors($stateParams.country);
+                },
+                thiscountry :  function(api, $stateParams) {
+                    $stateParams.thiscountry = api.searchThisCountryInfo($stateParams.country);
+                    return $stateParams.thiscountry
+                },
+                thiscapital :  function(api, thiscountry) {
+                    return  api.searchCapitals(thiscountry[0].countryCode, thiscountry[0].capital);
                 }
-            },
+            }
             
         });
 
@@ -73,118 +89,73 @@ angular.module('ccApp', ['ui.router', 'ngAnimate'])
             }
         };
     })
-.controller('countryCtrl', ['$scope', '$http', '$sce', '$state', function($scope, $http, $sce, $location, loadingState){
-    $scope.trustSrc = function(src) {
-    return $sce.trustAsResourceUrl(src);
-    }
-    $scope.importCountries = function() {
-        var url = "http://api.geonames.org/countryInfo";
-        var request = {
-            username: 'stzy',
-            type: 'JSON'
-        }
-        $scope.loading = true;
-        $http({
-            method: 'GET',
-            url: url,
-            params: request,
-            cache: true
-        })
-            .then(function (response) {
-                $scope.results = response.data.geonames;
-                console.log($scope.results);
-            },
-            function (response) {
-                alert('error');
-            })
-            .finally(function () {
-                // Hide loading spinner whether our call succeeded or failed.
-                $scope.loading = false;
-            });
-    }
-    //if no results run the api call
-    if(!$scope.results) {
-        $scope.importCountries();
-    }
+.controller('countryCtrl', ['$scope', '$http', 'api2', 'importcountries', '$state', function($scope, $http, api2, importcountries, $state){
+
+      $scope.importcountries = importcountries;
+      //  console.log(importcountries);
+
 
     $scope.goToDetail = function(cCode) {
         $state.go('country', {country: cCode.countryCode});
     };
 }])
-.controller('countryDetailCtrl', ['$scope', '$http', 'api', 'neighbors', function($scope, $http, api, neighbors){
+.controller('countryDetailCtrl', ['$scope', '$http', 'api', 'neighbors', 'thiscountry', 'thiscapital',  function($scope, $http, api, neighbors, thiscountry, thiscapital){
 
-        $scope.searchThisCountryInfo = function() {
-            var url = "http://api.geonames.org/countryInfo";
-            var request = {
-                username: 'stzy',
-                type: 'JSON',
-                country: $scope.country
-            };
-            $http({
-                method: 'GET',
-                url: url,
-                params: request,
-                cache: true
-            })
-                .then(function (response) {
-                    $scope.results3 = response.data.geonames;
-                    $scope.countryName = $scope.results3[0]['countryName'];
-                    $scope.population = $scope.results3[0]['population'];
-                    $scope.areaInSqKm = $scope.results3[0]['areaInSqKm'];
-                    $scope.capital = $scope.results3[0]['capital'];
-                    $scope.continent = $scope.results3[0]['continent'];
-
-                  //  continent, timezone
-                   // console.log(response.data.geonames);
-                },
-                function (response) {
-                    alert('error');
-                });
-        }
-
-        $scope.searchCapitals = function() {
-            var url = "http://api.geonames.org/search";
-            var request = {
-                username: 'stzy',
-                type: 'JSON',
-                name_equals :  $scope.capital,
-                q : 'capital',
-                country: $scope.country,
-                isNameRequired : 1,
-                maxRows : 1
-            };
-            $http({
-                method: 'GET',
-                url: url,
-                params: request,
-                cache: true
-            })
-                .then(function (response) {
-                    $scope.results = response.data.geonames;
-                    //console.log(response);
-                    if($scope.results[0]) {
-                        $scope.capPopulation = $scope.results[0]['population'];
-                        $scope.capName = $scope.results[0]['name'];
-                    }
-                    else{
-                        $scope.capPopulation = 'No Data';
-                        $scope.capName = 'No Data';
-                    }
-                },
-                function (response) {
-                    alert('error');
-                });
-        }
+        //$scope.searchThisCountryInfo = function() {
+        //    var url = "http://api.geonames.org/countryInfo";
+        //    var request = {
+        //        username: 'stzy',
+        //        type: 'JSON',
+        //        country: $scope.country
+        //    };
+        //    $http({
+        //        method: 'GET',
+        //        url: url,
+        //        params: request,
+        //        cache: true
+        //    })
+        //        .then(function (response) {
+        //            $scope.results3 = response.data.geonames;
+        //            $scope.countryName = $scope.results3[0]['countryName'];
+        //            $scope.population = $scope.results3[0]['population'];
+        //            $scope.areaInSqKm = $scope.results3[0]['areaInSqKm'];
+        //            $scope.capital = $scope.results3[0]['capital'];
+        //            $scope.continent = $scope.results3[0]['continent'];
+        //
+        //          //  continent, timezone
+        //           // console.log(response.data.geonames);
+        //        },
+        //        function (response) {
+        //            alert('error');
+        //        });
+        //}
 
 
-        $scope.searchNeighbors = function() {
-            $scope.searchResults = neighbors;
-            $scope.howMay = neighbors.length;
-        };
 
-        $scope.searchThisCountryInfo();
-        $scope.searchCapitals();
-        $scope.searchNeighbors();
+
+
+        //$scope.searchNeighbors = function() {
+        //    $scope.searchResults = neighbors;
+        //    $scope.howMay = neighbors.length;
+        //};
+
+        //api.searchThisCountryInfo().then(function(country) {
+        //    $scope.country = $stateParams.country;
+        //});
+        //$scope.country = api.searchThisCountryInfo();
+        //api.searchThisCountryInfo('US');
+        //rr = api.searchThisCountryInfo('US');
+
+
+        $scope.thiscountry = thiscountry[0];
+        $scope.neighbors = neighbors;
+        $scope.thiscapital = thiscapital[0];
+
+     //   $scope.thiscapital =  api.searchCapitals($scope.thiscountry.countryName, $scope.thiscountry.capital);
+
+      //  console.log(thiscapital);
+       // api.searchCapitals('US', 'Washington');
+
 
 
 }])
@@ -197,7 +168,10 @@ angular.module('ccApp', ['ui.router', 'ngAnimate'])
     };
 
     return {
-        searchNeighbors: searchNeighbors
+        searchNeighbors: searchNeighbors,
+        searchThisCountryInfo: searchThisCountryInfo,
+        searchCapitals: searchCapitals
+
     };
 
     function searchNeighbors(country){
@@ -205,11 +179,104 @@ angular.module('ccApp', ['ui.router', 'ngAnimate'])
             country: country
         };
         angular.extend(config.params, reqParams);
-        return $http.get(baseUrl + 'neighboursJSON', config)
+        return $http.get(baseUrl + 'neighboursJSON', config, {cache: true})
             .then(function(response){
                 return $q.when(response.data.geonames);
             });
     }
-});
+    function searchThisCountryInfo(country) {
+        var reqParams = {
+            country: country,
+            type: 'JSON'
+        };
+        angular.extend(config.params, reqParams);
+        return $http.get(baseUrl + 'countryInfo', config, {cache: true})
+            .then(function (response) {
+                return $q.when(response.data.geonames);
+
+                //
+                //$scope.countryName = $scope.results3[0]['countryName'];
+                //$scope.population = $scope.results3[0]['population'];
+                //$scope.areaInSqKm = $scope.results3[0]['areaInSqKm'];
+                //$scope.capital = $scope.results3[0]['capital'];
+                //$scope.continent = $scope.results3[0]['continent'];
+            });
+    }
+    function searchCapitals(country, capital) {
+        var reqParams = {
+            name_equals: capital,
+            q: 'capital',
+            country: country,
+            isNameRequired: 1,
+            maxRows: 1,
+            type: 'JSON'
+        };
+        angular.extend(config.params, reqParams);
+        return $http.get(baseUrl + 'search', config, {cache: true})
+            .then(function (response) {
+               // results = response.data.geonames;
+               //// console.log(response);
+               // if (results[0]) {
+               //     capPopulation = results[0]['population'];
+               //     capName = results[0]['name'];
+               // }
+               // else {
+               //     capPopulation = 'No Data';
+               //     capName = 'No Data';
+               // }
+                return $q.when(response.data.geonames);
+            });
+
+    }
+
+})
+
+//when I had this in the factory above it was being overritten by searchThisCountryInfo when you went back to the list after loading the detail page
+.factory('api2', function($http, $q){
+
+    var baseUrl = 'http://api.geonames.org/';
+    var config = {
+        params: {
+            username: 'stzy'
+        }
+    };
+
+    return {
+        importCountries : importCountries
+
+    };
+    function importCountries() {
+
+            var reqParams = {
+                type: 'JSON'
+            };
+            angular.extend(config.params, reqParams);
+            return $http.get(baseUrl + 'countryInfo', config, {cache: true})
+                .then(function (response) {
+                    //if there's a cached version saved in api2 don't load a new one when the state is loaded
+                    var cache;
+                    if(!cache) {
+                        cache = $q.when(response.data.geonames);
+                    }
+                    return cache;
+                });
+    }
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
 
 
